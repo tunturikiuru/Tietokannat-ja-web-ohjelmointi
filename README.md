d56bf57e6e54d6314acc4e72d4d5049ac59d20a8
Anna Salminiitty <koivu05@gmail.com>
2024-07-27 20:52:15 +0300

    Headings and subforums

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 kurssin "Tietokannat ja web-ohjelmointi" materiaalin perusteella itsenäisesti tehty harjoitustyö
+
+Tietokanta
+
+CREATE TABLE headings (
+    heading_id SERIAL PRIMARY KEY, 
+    heading_name TEXT,
+    order_index INTEGER UNIQUE);
+
+CREATE TABLE subforums (
+    subforum_id SERIAL PRIMARY KEY, 
+    subforum_name TEXT NOT NULL,
+    heading_id INTEGER REFERENCES headings(heading_id), 
+    order_index INTEGER UNIQUE
+    private BOOLEAN DEFAULT FALSE);
+
--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import redirect, render_template, request
+from flask import redirect, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import text
 
@@ -10,20 +10,62 @@ db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
-    result = db.session.execute(text("SELECT name, subtitle FROM forums"))
-    forums = result.fetchall()
-    return render_template("index.html", forums=forums) 
+    result = db.session.execute(text("SELECT heading_name FROM headings WHERE order_index < 3 ORDER BY order_index"))
+    headings= result.fetchall()
+    result = db.session.execute(text("SELECT h.order_index h_order, h.heading_name h_name, s.subforum_name s_name, s.subforum_id s_id FROM subforums s LEFT JOIN headings h ON h.heading_id = s. heading_id ORDER BY h.order_index, s.order_index"))
+    subforums = result.fetchall()
+    subforum_order = {}
+    for subforum in subforums:
+        if (subforum.h_order, subforum.h_name) not in subforum_order:
+            subforum_order[(subforum.h_order, subforum.h_name)] = []
+        subforum_order[(subforum.h_order, subforum.h_name)].append((subforum.s_name, subforum.s_id))
+    return render_template("index.html", headings=headings, subforum_order=subforum_order) 
 
-@app.route("/new")
-def new():
-    return render_template("new.html")
+@app.route("/settings")
+def settings():
+    return render_template("settings.html")
 
-@app.route("/send", methods=["POST"])
-def send():
-    name = request.form["title"]
+@app.route("/settings/send", methods=["POST"])
+def send_settings():
+    title = request.form["title"]
+    if title != "":
+        sql = "INSERT INTO headings (heading_name, order_index) VALUES (:title, 1) ON CONFLICT (order_index) DO UPDATE SET heading_name = :title"
+        db.session.execute(text(sql), {"title":title})
+        db.session.commit()
     subtitle = request.form["subtitle"]
-    alias = request.form["alias"]
-    sql = "INSERT INTO forums (name, subtitle, alias) VALUES (:name, :subtitle, :alias)"
-    db.session.execute(text(sql), {"name":name, "subtitle":subtitle, "alias":alias})
+    if subtitle != "":
+        sql = "INSERT INTO headings (heading_name, order_index) VALUES (:subtitle, 2) ON CONFLICT (order_index) DO UPDATE SET heading_name = :subtitle"
+        db.session.execute(text(sql), {"subtitle":subtitle})
+        db.session.commit()
+    private = request.form["private"]
+    if private != "":
+        sql = "INSERT INTO headings (heading_name, order_index) VALUES (:private, 3) ON CONFLICT (order_index) DO UPDATE SET heading_name = :private"
+        db.session.execute(text(sql), {"private":private})
+        db.session.commit()
+    headings = request.form.getlist("heading")
+    order_index = 4
+    for heading in headings:
+        if heading != "":
+            sql = "INSERT INTO headings (heading_name, order_index) VALUES (:heading, :order_index) ON CONFLICT (order_index) DO UPDATE SET heading_name = :heading"
+            db.session.execute(text(sql), {"heading":heading, "order_index":order_index})
+            db.session.commit()
+        order_index += 1
+    return redirect("/")
+
+@app.route("/new_subforum")
+def new_subforum():
+    sql = "SELECT heading_name, heading_id FROM headings WHERE order_index >= 3 ORDER BY order_index"
+    result = db.session.execute(text(sql))
+    headings = result.fetchall()
+    return render_template("new_subforum.html", headings=headings)
+
+@app.route("/subforum/send", methods=["POST"])
+def send_subforum():
+    subforum = request.form["subforum"]
+    area = request.form["area"]
+    sql = "INSERT INTO subforums (heading_id, subforum_name) VALUES (:area, :subforum)"
+    db.session.execute(text(sql), {"area":area, "subforum":subforum})
     db.session.commit()
     return redirect("/")
+
+
