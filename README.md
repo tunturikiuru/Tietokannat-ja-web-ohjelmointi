0b99b591a00d19e78d3b4340d3949bee2f86e61e
Anna Salminiitty <koivu05@gmail.com>
2024-08-03 15:03:34 +0300

    Settings - headings and subforums - create, rename and change order

--- README.md
+++ README.md
@@ -1,16 +1,9 @@
-kurssin "Tietokannat ja web-ohjelmointi" materiaalin perusteella itsenäisesti tehty harjoitustyö
+Tämä on kurssin "Tietokannat ja web-ohjelmointi" -materiaalin perusteella itsenäisesti tehty harjoitustyö
 
-Tietokanta
+# Projekti
+Keskustelufoorumi, josta on tarkoitus tehdä helposti muokattava.
 
-CREATE TABLE headings (
-    heading_id SERIAL PRIMARY KEY, 
-    heading_name TEXT,
-    order_index INTEGER UNIQUE);
 
-CREATE TABLE subforums (
-    subforum_id SERIAL PRIMARY KEY, 
-    subforum_name TEXT NOT NULL,
-    heading_id INTEGER REFERENCES headings(heading_id), 
-    order_index INTEGER UNIQUE
-    private BOOLEAN DEFAULT FALSE);
+
+
 
--- app.py
+++ app.py
@@ -10,21 +10,29 @@ db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
-    result = db.session.execute(text("SELECT heading_name FROM headings WHERE order_index < 3 ORDER BY order_index"))
-    headings= result.fetchall()
-    result = db.session.execute(text("SELECT h.order_index h_order, h.heading_name h_name, s.subforum_name s_name, s.subforum_id s_id FROM subforums s LEFT JOIN headings h ON h.heading_id = s. heading_id ORDER BY h.order_index, s.order_index"))
-    subforums = result.fetchall()
-    subforum_order = {}
-    for subforum in subforums:
-        if (subforum.h_order, subforum.h_name) not in subforum_order:
-            subforum_order[(subforum.h_order, subforum.h_name)] = []
-        subforum_order[(subforum.h_order, subforum.h_name)].append((subforum.s_name, subforum.s_id))
-    return render_template("index.html", headings=headings, subforum_order=subforum_order) 
+    titles = fetch_title()
+    subforum_order = subforum_dict()
+    return render_template("index.html", titles=titles, subforum_order=subforum_order) 
 
+# Settings
 @app.route("/settings")
 def settings():
-    return render_template("settings.html")
+    titles = fetch_title()
+    return render_template("settings.html", titles=titles)
 
+@app.route("/settings/areas")
+def area_settings():
+    headings = fetch_areas()
+    return render_template("area_settings.html", headings=headings)
+
+@app.route("/settings/subforums")
+def subforums():
+    headings = fetch_areas()
+    subforums_list = subforum_list()
+    subforums_dict = subforum_dict()
+    return render_template("subforum_settings.html", headings=headings, subforums_list=subforums_list, subforums_dict=subforums_dict)
+
+# Settings send
 @app.route("/settings/send", methods=["POST"])
 def send_settings():
     title = request.form["title"]
@@ -37,35 +45,135 @@ def send_settings():
         sql = "INSERT INTO headings (heading_name, order_index) VALUES (:subtitle, 2) ON CONFLICT (order_index) DO UPDATE SET heading_name = :subtitle"
         db.session.execute(text(sql), {"subtitle":subtitle})
         db.session.commit()
-    private = request.form["private"]
-    if private != "":
-        sql = "INSERT INTO headings (heading_name, order_index) VALUES (:private, 3) ON CONFLICT (order_index) DO UPDATE SET heading_name = :private"
-        db.session.execute(text(sql), {"private":private})
-        db.session.commit()
-    headings = request.form.getlist("heading")
-    order_index = 4
-    for heading in headings:
-        if heading != "":
-            sql = "INSERT INTO headings (heading_name, order_index) VALUES (:heading, :order_index) ON CONFLICT (order_index) DO UPDATE SET heading_name = :heading"
-            db.session.execute(text(sql), {"heading":heading, "order_index":order_index})
-            db.session.commit()
-        order_index += 1
+    else:
+        pass
+        # POISTA 
     return redirect("/")
 
-@app.route("/new_subforum")
-def new_subforum():
+@app.route("/settings/area_name/send", methods=["POST"])
+def send_new_area():
+    heading = request.form["heading"]
+    if heading != "":
+        order_index = len(fetch_areas()) + 3
+        sql = "INSERT INTO headings (heading_name, order_index) VALUES (:heading, :order_index)"
+        db.session.execute(text(sql), {"heading":heading, "order_index":order_index})
+        db.session.commit()
+    return redirect("/settings/areas")
+
+@app.route("/settings/area_rename/send", methods=["POST"])
+def send_change_area_name():
+    heading_id = request.form["old_heading"]
+    new_name = request.form["new_heading"]
+    if new_name != "":
+        sql = "UPDATE headings SET heading_name = :new_name WHERE heading_id = :heading_id"
+        db.session.execute(text(sql), {"new_name":new_name, "heading_id":heading_id})
+        db.session.commit()
+    return redirect("/settings/areas")
+
+
+@app.route("/settings/area_order/send", methods=["POST"])
+def send_area_order():
+    heading_order = request.form.getlist("heading_order")
+    heading_ids = request.form.getlist("heading_id")
+    update_order_index(heading_order, heading_ids, "heading")
+    return redirect("/")
+
+@app.route("/settings/delete_area/send", methods=["POST"])
+def send_delete_area():
+    pass
+
+@app.route("/new_subforum/send", methods=["POST"])
+def send_subforum():
+    subforum = request.form["new_subforum"]
+    area = request.form["area"]
+    result = db.session.execute(text("SELECT COUNT(subforum_id) FROM subforums WHERE heading_id=:area"), {"area":area})
+    order_index = result.fetchone()[0]+1
+    sql = "INSERT INTO subforums (heading_id, subforum_name, order_index) VALUES (:area, :subforum, :order_index)"
+    db.session.execute(text(sql), {"area":area, "subforum":subforum, "order_index":order_index})
+    db.session.commit()
+    return redirect("/settings/subforums")
+
+@app.route("/settings/subforum_rename/send", methods=["POST"])
+def rename_subforum():
+    subforum_id = request.form["old_name"]
+    name = request.form["new_name"]
+    if name != "":
+        sql = "UPDATE subforums SET subforum_name =:name WHERE subforum_id =:subforum_id"
+        db.session.execute(text(sql), {"name":name, "subforum_id":subforum_id})
+        db.session.commit()
+    return redirect("/settings/subforums")
+
+@app.route("/settings/subforum_order/send", methods=["POST"])
+def update_subforum_order():
+    subforum_order = request.form.getlist("subforum_order")
+    print(subforum_order)
+    subforum_ids = request.form.getlist("subforum_id")
+    print(subforum_ids)
+    update_order_index(subforum_order, subforum_ids, "subforum")
+    return redirect("/settings/subforums")
+    #    order = sorted(zip(subforum_order, subforums))
+    #    order = [x[1] for x in order]
+    #    for i in range(len(order)):
+    #        #temporary index
+    #        sql = "UPDATE subforums SET "
+
+# database functions
+def fetch_title():
+    sql = "SELECT heading_name, heading_id FROM headings WHERE order_index < 3 ORDER BY order_index"
+    result = db.session.execute(text(sql))
+    titles = result.fetchall()
+    while len(titles) <2:
+        titles.append(("", ""))
+    return titles
+
+def fetch_areas():
     sql = "SELECT heading_name, heading_id FROM headings WHERE order_index >= 3 ORDER BY order_index"
     result = db.session.execute(text(sql))
     headings = result.fetchall()
-    return render_template("new_subforum.html", headings=headings)
+    return headings
 
-@app.route("/subforum/send", methods=["POST"])
-def send_subforum():
-    subforum = request.form["subforum"]
-    area = request.form["area"]
-    sql = "INSERT INTO subforums (heading_id, subforum_name) VALUES (:area, :subforum)"
-    db.session.execute(text(sql), {"area":area, "subforum":subforum})
-    db.session.commit()
-    return redirect("/")
+def subforum_by_heading(heading_id):
+    sql = "SELECT subforum_name, subforum_id FROM subforums WHERE heading_id=:heading_id ORDER BY order_index"
+    result = db.session.execute(text(sql), {"heading_id":heading_id})
+    subforums = result.fetchall()
+    return subforums
+    
+def subforum_list():
+    sql = "SELECT subforum_name, subforum_id FROM subforums ORDER BY subforum_id, order_index"
+    result = db.session.execute(text(sql))
+    subforums = result.fetchall()
+    return subforums
+
+def subforum_dict():
+    sql = "SELECT h.heading_name h_name, h.heading_id h_id, s.subforum_name s_name, s.subforum_id s_id FROM subforums s LEFT JOIN headings h ON h.heading_id = s. heading_id ORDER BY h.order_index, s.order_index"
+    result = db.session.execute(text(sql))
+    subforums = result.fetchall()
+    subforum_order = {}
+    for subforum in subforums:
+        if (subforum.h_name, subforum.h_id) not in subforum_order:
+            subforum_order[(subforum.h_name, subforum.h_id)] = []
+        subforum_order[(subforum.h_name, subforum.h_id)].append((subforum.s_name, subforum.s_id))
+    return subforum_order
+
+def update_order_index(order_index: list, ids: list, category: str):
+    order = sorted(zip(order_index, ids))
+    if category == "heading":
+        sql = "UPDATE headings SET order_index = :i WHERE heading_id =:id"
+        order = [(index+3, x[1]) for index, x in enumerate(order)]
+    if category == "subforum":
+        sql = "UPDATE subforums SET order_index = :i WHERE subforum_id =:id"
+        order = [(index+1, x[1]) for index, x in enumerate(order)]
+    # temporary index
+    for item in order:
+        print(item)
+        db.session.execute(text(sql), {"i":item[0]+100000, "id":item[1]})
+    # permanent index
+    for item in order:
+        db.session.execute(text(sql), {"i":item[0], "id":item[1]})
+    db.session.commit() 
+
+
+
+
 
 
