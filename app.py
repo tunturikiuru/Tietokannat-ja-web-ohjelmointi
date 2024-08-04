4b28c73dceae919452d1575571507db2fdc25430
Anna Salminiitty <koivu05@gmail.com>
2024-08-04 11:18:09 +0300

    New topics and new messages, schema.sql

--- app.py
+++ app.py
@@ -14,6 +14,47 @@ def index():
     subforum_order = subforum_dict()
     return render_template("index.html", titles=titles, subforum_order=subforum_order) 
 
+@app.route("/subforum/<int:subforum_id>")
+def subforum(subforum_id):
+    subforum = fetch_subforum_by_id(subforum_id)
+    topics = fetch_topics(subforum_id)
+    return render_template("subforum.html", subforum=subforum, topics=topics)
+
+@app.route("/subforum/<int:id>/new_topic")
+def create_new_topic(id):
+    return render_template("new_topic.html", id=id)
+
+
+@app.route("/subforum/<int:id>/new_topic/send", methods=["POST"])
+def send_new_topic(id):
+    title = request.form["title"]
+    message = request.form["content"]
+    id = new_topic(title, message, id)
+    return redirect(url_for("topic", topic_id=id))
+
+@app.route("/topic/<int:topic_id>")
+def topic(topic_id):
+    subforum = fetch_subforum_by_topic(topic_id)
+    topic = fetch_topic(topic_id)
+    messages = fetch_messages(topic_id)
+    print(messages)
+    return render_template("topic.html", messages=messages, subforum=subforum, topic=topic)
+
+@app.route("/topic/<int:topic_id>/new_message/")
+def new_message(topic_id):
+    topic = fetch_topic(topic_id)
+    return render_template("new_message.html", topic=topic)
+
+@app.route("/new_message/send", methods=["POST"])
+def send_new_message():
+    message = request.form["message"]
+    print(message)
+    topic_id = request.form["topic_id"]
+    print(topic_id)
+    new_message(topic_id, message)
+    return redirect(url_for("topic", topic_id=topic_id))
+
+
 # Settings
 @app.route("/settings")
 def settings():
@@ -22,12 +63,12 @@ def settings():
 
 @app.route("/settings/areas")
 def area_settings():
-    headings = fetch_areas()
+    headings = fetch_headings()
     return render_template("area_settings.html", headings=headings)
 
 @app.route("/settings/subforums")
 def subforums():
-    headings = fetch_areas()
+    headings = fetch_headings()
     subforums_list = subforum_list()
     subforums_dict = subforum_dict()
     return render_template("subforum_settings.html", headings=headings, subforums_list=subforums_list, subforums_dict=subforums_dict)
@@ -47,14 +88,14 @@ def send_settings():
         db.session.commit()
     else:
         pass
-        # POISTA 
+        # POISTA ALAOTSIKKO
     return redirect("/")
 
 @app.route("/settings/area_name/send", methods=["POST"])
 def send_new_area():
     heading = request.form["heading"]
     if heading != "":
-        order_index = len(fetch_areas()) + 3
+        order_index = len(fetch_headings()) + 3
         sql = "INSERT INTO headings (heading_name, order_index) VALUES (:heading, :order_index)"
         db.session.execute(text(sql), {"heading":heading, "order_index":order_index})
         db.session.commit()
@@ -106,18 +147,27 @@ def rename_subforum():
 @app.route("/settings/subforum_order/send", methods=["POST"])
 def update_subforum_order():
     subforum_order = request.form.getlist("subforum_order")
-    print(subforum_order)
     subforum_ids = request.form.getlist("subforum_id")
-    print(subforum_ids)
     update_order_index(subforum_order, subforum_ids, "subforum")
     return redirect("/settings/subforums")
-    #    order = sorted(zip(subforum_order, subforums))
-    #    order = [x[1] for x in order]
-    #    for i in range(len(order)):
-    #        #temporary index
-    #        sql = "UPDATE subforums SET "
 
-# database functions
+# DATABASE FUNCTIONS
+# new
+def new_topic(topic_id, message, subforum_id):
+    sql = "INSERT INTO topics (subforum_id, topic_name, created, updated) VALUES (:subforum_id, :topic, NOW(), NOW()) RETURNING topic_id"
+    result = db.session.execute(text(sql), {"subforum_id":subforum_id, "topic":topic_id})
+    topic_id = result.fetchone()
+    sql = "INSERT INTO messages (topic_id, message, time) VALUES (:topic_id, :message, NOW())"
+    db.session.execute(text(sql), {"topic_id":topic_id[0], "message":message})
+    db.session.commit()
+    return topic_id[0]
+
+def new_message(topic_id, message):
+    sql = "INSERT INTO messages (topic_id, message, time) VALUES (:topic_id, :message, NOW())"
+    db.session.execute(text(sql), {"topic_id":topic_id, "message":message})
+    db.session.commit()
+
+#fetch
 def fetch_title():
     sql = "SELECT heading_name, heading_id FROM headings WHERE order_index < 3 ORDER BY order_index"
     result = db.session.execute(text(sql))
@@ -126,17 +176,51 @@ def fetch_title():
         titles.append(("", ""))
     return titles
 
-def fetch_areas():
+def fetch_headings():
     sql = "SELECT heading_name, heading_id FROM headings WHERE order_index >= 3 ORDER BY order_index"
     result = db.session.execute(text(sql))
     headings = result.fetchall()
     return headings
 
-def subforum_by_heading(heading_id):
-    sql = "SELECT subforum_name, subforum_id FROM subforums WHERE heading_id=:heading_id ORDER BY order_index"
-    result = db.session.execute(text(sql), {"heading_id":heading_id})
-    subforums = result.fetchall()
-    return subforums
+def fetch_subforum_by_topic(topic_id):
+    sql = "SELECT s.subforum_name, s.subforum_id FROM topics t LEFT JOIN subforums s ON t.subforum_id=s.subforum_id WHERE t.topic_id=:topic_id"
+    result = db.session.execute(text(sql), {"topic_id":topic_id})
+    topics = result.fetchone()
+    return topics
+
+def fetch_subforum_by_id(subforum_id):
+    sql = "SELECT subforum_name, subforum_id FROM subforums WHERE subforum_id=:subforum_id"
+    result = db.session.execute(text(sql), {"subforum_id":subforum_id})
+    topics = result.fetchone()
+    return topics
+
+# TODO VOIKO TRY-EXCEPT poistaa?
+def fetch_topics(id):
+    try:
+        sql = "SELECT topic_name, topic_id FROM topics WHERE subforum_id=:id ORDER BY pinned, updated"
+        result = db.session.execute(text(sql), {"id":id})
+        topics = result.fetchall()
+    except:
+        topics = []
+    return topics
+
+def fetch_topic(topic_id):
+    sql = "SELECT topic_name, topic_id FROM topics WHERE topic_id=:topic_id ORDER BY pinned, updated"
+    result = db.session.execute(text(sql), {"topic_id":topic_id})
+    topic = result.fetchone()
+    return topic
+
+def fetch_messages(topic_id):
+    sql = "SELECT message, time FROM messages WHERE topic_id=:topic_id ORDER BY time"
+    result = db.session.execute(text(sql), {"topic_id":topic_id})
+    messages = result.fetchall()
+    return messages
+
+#def subforum_by_heading(heading_id):
+#    sql = "SELECT subforum_name, subforum_id FROM subforums WHERE heading_id=:heading_id ORDER BY order_index"
+#    result = db.session.execute(text(sql), {"heading_id":heading_id})
+#    subforums = result.fetchall()
+#    return subforums
     
 def subforum_list():
     sql = "SELECT subforum_name, subforum_id FROM subforums ORDER BY subforum_id, order_index"
@@ -155,6 +239,7 @@ def subforum_dict():
         subforum_order[(subforum.h_name, subforum.h_id)].append((subforum.s_name, subforum.s_id))
     return subforum_order
 
+# update
 def update_order_index(order_index: list, ids: list, category: str):
     order = sorted(zip(order_index, ids))
     if category == "heading":
---
+++ schema.sql
@@ -0,0 +1,27 @@
+CREATE TABLE headings (
+    heading_id SERIAL PRIMARY KEY, 
+    heading_name TEXT,
+    order_index INTEGER UNIQUE);
+
+CREATE TABLE subforums (
+    subforum_id SERIAL PRIMARY KEY, 
+    subforum_name TEXT NOT NULL,
+    heading_id INTEGER REFERENCES headings(heading_id), 
+    order_index INTEGER,
+    private BOOLEAN DEFAULT FALSE,
+    UNIQUE (heading_id, order_index));
+
+CREATE TABLE topics (
+    topic_id SERIAL PRIMARY KEY, 
+    subforum_id INTEGER REFERENCES subforums(subforum_id), 
+    topic_name TEXT NOT NULL,
+    created TIMESTAMP, 
+    updated TIMESTAMP,
+    pinned BOOLEAN DEFAULT FALSE);
+
+CREATE TABLE messages (
+	message_id SERIAL PRIMARY KEY,
+	topic_id INTEGER REFERENCES topics(topic_id),
+	message TEXT,
+	sender TEXT,
+	time TIMESTAMP);
